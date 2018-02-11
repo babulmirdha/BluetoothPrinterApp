38076f2b27a70385dabc4b11ad9813fb94d70787
Babul Mirdha <babumirdha@gmail.com>
2018-02-11 19:23:09 +0600

    Added some new field

--- app/src/main/java/com/example/bluetoothprinterapp/BluetoothOldPrintActivity.java
+++ app/src/main/java/com/example/bluetoothprinterapp/BluetoothOldPrintActivity.java
@@ -1,41 +1,32 @@
 package com.example.bluetoothprinterapp;
-
-import android.app.Activity;
+       
+import android.app.Activity;       
+import android.os.Bundle;       
+import java.io.IOException;
+import java.io.OutputStream;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
-import java.io.IOException;
-import java.io.OutputStream;
-
-
-public class BluetoothPrintActivity extends AppCompatActivity
+public class BluetoothOldPrintActivity extends AppCompatActivity
 {       
     /** Called when the activity is first created. */       
-
+    EditText message;
     Button printbtn;
 
 	byte FONT_TYPE;
 	private static BluetoothSocket btsocket;
 	private static OutputStream btoutputstream;
-	private EditText accountNoEditText;
-	private EditText accountNameEditText;
-	private EditText amountEditText;
-
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_print_bluetooth);
+		setContentView(R.layout.main);
+		message = (EditText)findViewById(R.id.message);
 		printbtn = (Button)findViewById(R.id.printButton);
-		accountNoEditText = (EditText) findViewById(R.id.accountNoEditText);
-		accountNameEditText = (EditText) findViewById(R.id.accountNameEditText);
-		amountEditText = (EditText) findViewById(R.id.amountEditText);
-
 
 		printbtn.setOnClickListener(new OnClickListener() {
 
@@ -79,7 +70,7 @@ public class BluetoothPrintActivity extends AppCompatActivity
 
 			byte[] printformat = { 0x1B, 0x21, FONT_TYPE };
 			btoutputstream.write(printformat);
-			String msg = getMessage();
+			String msg = message.getText().toString();
 			btoutputstream.write(msg.getBytes());
 			btoutputstream.write(0x0D);
 			btoutputstream.write(0x0D);
@@ -117,23 +108,5 @@ public class BluetoothPrintActivity extends AppCompatActivity
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}
-
-	private String getMessage() {
-		StringBuilder stringBuilder = new StringBuilder();
-
-		double amount = Double.parseDouble(amountEditText.getText().toString());
-		stringBuilder.append(String.format("Deposited amount: %.2f\n",amount));
-
-		stringBuilder.append(String.format("Account No: %s\n",accountNoEditText.getText().toString()));
-
-		stringBuilder.append(String.format("Account Name: %s\n",accountNameEditText.getText().toString()));
-
-
-		stringBuilder.append(String.format("Printed by: %s\n","Babul"));
-
-		stringBuilder.append("\n\n\n\n\n");
-
-		return stringBuilder.toString();
-	}
+	}  
 }         
