8b3aefa9457a10ff0665000ddbf718f2ae3371a4
Babul Mirdha <babumirdha@gmail.com>
2022-07-28 23:56:31 +0600

    BTDeviceListActivity.kt

---
+++ app/src/main/java/com/example/bluetoothprinterapp/BTDeviceListActivity.kt
@@ -0,0 +1,75 @@
+package com.example.bluetoothprinterapp
+
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.widget.ArrayAdapter
+import android.os.Bundle
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.TextView
+import android.content.Intent
+import android.util.Log
+import android.view.View
+import android.view.Window
+import android.widget.ListView
+import java.lang.Exception
+
+/**
+ * Created by hp on 12/23/2016.
+ */
+class BTDeviceListActivity : Activity() {
+    private var mBluetoothAdapter: BluetoothAdapter? = null
+    private var mPairedDevicesArrayAdapter: ArrayAdapter<String>? = null
+    override fun onCreate(mSavedInstanceState: Bundle?) {
+        super.onCreate(mSavedInstanceState)
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS)
+        setContentView(R.layout.bt_device_list)
+        setResult(RESULT_CANCELED)
+        mPairedDevicesArrayAdapter = ArrayAdapter(this, R.layout.bt_device_name)
+        val mPairedListView = findViewById<View>(R.id.paired_devices) as ListView
+        mPairedListView.adapter = mPairedDevicesArrayAdapter
+        mPairedListView.onItemClickListener = mDeviceClickListener
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        val mPairedDevices = mBluetoothAdapter?.getBondedDevices()
+        if (mPairedDevices?.size!! > 0) {
+            findViewById<View>(R.id.title_paired_devices).visibility = View.VISIBLE
+            for (mDevice in mPairedDevices) {
+                mPairedDevicesArrayAdapter!!.add("""
+    ${mDevice.name}
+    ${mDevice.address}
+    """.trimIndent())
+            }
+        } else {
+            val mNoDevices =
+                "None Paired" //getResources().getText(R.string.none_paired).toString();
+            mPairedDevicesArrayAdapter!!.add(mNoDevices)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (mBluetoothAdapter != null) {
+            mBluetoothAdapter!!.cancelDiscovery()
+        }
+    }
+
+    private val mDeviceClickListener =
+        OnItemClickListener { mAdapterView, mView, mPosition, mLong ->
+            try {
+                mBluetoothAdapter!!.cancelDiscovery()
+                val mDeviceInfo = (mView as TextView).text.toString()
+                val mDeviceAddress = mDeviceInfo.substring(mDeviceInfo.length - 17)
+                Log.v(TAG, "Device_Address $mDeviceAddress")
+                val mBundle = Bundle()
+                mBundle.putString("DeviceAddress", mDeviceAddress)
+                val mBackIntent = Intent()
+                mBackIntent.putExtras(mBundle)
+                setResult(RESULT_OK, mBackIntent)
+                finish()
+            } catch (ex: Exception) {
+            }
+        }
+
+    companion object {
+        protected const val TAG = "TAG"
+    }
+}
